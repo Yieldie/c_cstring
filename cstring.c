c4f4a2f7adece5eb7eac883ec9cfaa9c43389794
yieldie <kacperkrasnal@gmail.com>
2021-12-04 14:41:26 +0100

    Add comments

--- cstring.c
+++ cstring.c
@@ -8,6 +8,9 @@ typedef struct CString
 	int lock;
 } CString;
 
+/**
+ * Funkcja zwraca nową strukturę CString z zawartością str.
+ */
 CString *cstring_new(const char* str)
 {
 	int l = 0;
@@ -22,16 +25,26 @@ CString *cstring_new(const char* str)
 	return ret;
 }
 
+/**
+ *Funkcja zwraca długość stringa str.
+ */
 int length(CString *str)
 {
 	return str->len;
 }
 
+/**
+ * Funkcja zwraca znak na pozycji pos w stringu str, o ile pos mieści się w zakresie.
+ */
 char get_char(CString *str, int pos)
 {
 	return (pos < str->len) ? *(str->content + pos) : '\0';
 }
 
+/**
+ * Funkcja zmienia znak na pozycji pos w stringu str na znak sub, o ile zmiana jest dozwolona 
+ * i pos mieści się w zakresie.
+ */
 void replace(CString *str, int pos, char sub)
 {
 	if(str->lock == UNLOCKED && pos < str->len) {
@@ -39,6 +52,9 @@ void replace(CString *str, int pos, char sub)
 	}
 }
 
+/**
+ * Funkcja zwraca reprezentację char* struktury str i nakłada blokadę modyfikacji (sprawdzaną w procedurze replace()).
+ */
 char *to_char_ptr(CString *str)
 {
 	str->lock = LOCKED;
